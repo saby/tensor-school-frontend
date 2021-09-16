0eb91a33b21f1c201cf54ec0e3c3d939af7675fd
as.zaycev <as.zaycev@tensor.ru>
2021-09-16 10:26:15 +0300

    пример падающих тестов и тестов вообще

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,17 @@
-import { sum } from './index';
+import { sum, sortAndSplit, addsDivToBody } from './index';
 
-test('пример теста', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('название урока', () => {
+  test('сумма 1 и 2', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('test', () => {
+    expect(sortAndSplit('1,5,4')).toStrictEqual(['1', '4', '5']);
+  });
+
+  test('test1', () => {
+    addsDivToBody('as');
+
+    expect(document.body).toMatchSnapshot();
+  });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,13 @@
 export function sum(a: number, b: number): number {
-  return a+b;
+  return a + b;
+}
+
+export function sortAndSplit(a: string): string[] {
+  return a.split(",").sort((a, b) => parseInt(a, 10) - parseInt(b, 10));
+}
+
+export function addsDivToBody(a: string): void {
+  const child = document.createElement('div');
+  child.textContent = a;
+  document.body.appendChild(child);
 }
