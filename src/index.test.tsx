15a7b439e88dbdcb29b82c63ce58b4961a909d93
vv.shumilov <vv.shumilov@tensor.ru>
2021-09-17 18:07:14 +0300

    https://online.sbis.ru/opendoc.html?guid=a394cc38-d41a-45df-b160-b66a32e23a5c

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,15 @@
-import { sum } from './index';
+import { getDelimeters, lettersReplace, arraySum } from './index';
 
-test('пример теста', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('Урок 5.14 - Рекурсия', () => {
+  test('Рекурсивное вычисление суммы элементов массива', () => {
+    expect(arraySum([2, 12, 33, 0, 7, 17])).toBe(71);
+  });
+
+  test('Рекурсивное вычисление делителей числа 80', () => {
+    expect(getDelimeters(80)).toStrictEqual([2, 4, 5, 8, 10, 16, 20, 40]);
+  });
+
+  test('Рекурсивная очистка строки от дублирующихся пробелов', () => {
+    expect(lettersReplace("Я   встретил вас, и  все  былое...")).toBe("Я встретил вас, и все былое...");
+  }); 
+}); 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,52 @@
-export function sum(a: number, b: number): number {
-  return a+b;
+/*
+  Используя рекурсию, вычислите сумму всех элементов одномерного массива
+*/
+export function arraySum(arr: number[]): number {
+  let result = 0;
+  const sum = (pos: number): number => {
+    return arr[pos] + (pos < (arr.length - 1) ? sum(pos + 1) : 0);  
+  };
+  return sum(0);
+}
+
+
+/*
+  В функцию getDelimeters() приходит целое неотрицательное число.
+  Используя рекурсию, вычислите все делители этого числа, за исключением единицы и его самого,
+  и верните итоговый результат в виде массива чисел, упорядоченных по возрастанию.
+*/
+export function getDelimeters(value: number): number[] {
+  const result: number[] = [];
+  const getDelim = (delim: number) => {
+    if (delim < value) {
+      if (value % delim == 0) {
+        result.push(delim);
+      }
+      delim++;
+      getDelim(delim);
+    }
+  };
+  getDelim(2);
+  return result;
+}
+
+
+/*
+  В функцию lettersReplace() приходит строковая переменная, содержащая текст, где могут встречаться 
+  удвоенные, утроенные и т.д. пробелы. Используя рекурсию и не используя циклы или строковые функции автозамены, 
+  замените все неодиночные пробелы на одиночные и верните итоговую строку.
+*/
+export function lettersReplace(message: string): string {
+  let result = '';
+  const replaceStr = (pos: number) => {
+    if (pos > 0 && (message[pos] == ' ' && message[pos-1] !== ' ') || message[pos] !== ' ') {
+      result += message[pos];
+    }
+    if (pos < message.length - 1) {
+      pos++;
+      replaceStr(pos);
+    }
+  };
+  replaceStr(0);
+  return result;
 }
