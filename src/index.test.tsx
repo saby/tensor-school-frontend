26296d513b3130c27f0c1fe47cf9b8c52d5ac960
dm.kabanov <dm.kabanov@tensor.ru>
2021-09-20 16:35:06 +0300

    https://online.sbis.ru/opendoc.html?guid=5b5bedef-cd4a-4885-8cd9-2eac16da9ec5

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,21 @@
-import { sum } from './index';
+import {mock, getData, catchExeption, FIRST_DELAY, SECOND_DELAY, THIRD_DELAY} from './index';
 
-test('пример теста', () => {
-  expect(sum(1, 2)).toBe(3);
+test('Создание разрешенного промиса', () => {
+    return mock(FIRST_DELAY).then((data) => {
+        expect(data).toBe(FIRST_DELAY);
+    });
+});
+
+test('Параллельное выполнение промисов', () => {
+    const startTime = Date.now();
+    return getData().then((data) => {
+        expect(data).toEqual([FIRST_DELAY, SECOND_DELAY, THIRD_DELAY]);
+        expect(Date.now() - startTime).toBeLessThan(FIRST_DELAY + SECOND_DELAY + THIRD_DELAY);
+    });
+});
+
+test('Обработка ошибок в асинхронном коде', () => {
+    return catchExeption().then((res) => {
+        expect(res).toBe('my error');
+    });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,57 @@
-export function sum(a: number, b: number): number {
-  return a+b;
+export const FIRST_DELAY = 100;
+export const SECOND_DELAY = 200;
+export const THIRD_DELAY = 300;
+
+// 1.
+// напишите функцию, которая принимает на вход number аргумент (количество миллисекунд)
+// и возвращает промис, который завершится через заданное количество миллисекунд
+export function mock(ms: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
+}
+
+// 2.
+// Перепишите функцию так, чтобы она выполнялась быстрее.
+export function getData(): Promise<[number, number, number]> {
+    // const result = [];
+
+    // return mock(100)
+    //     .then((data1) => {
+    //         result.push(data1);
+    //         return mock(200);
+    //     })
+    //     .then((data2) => {
+    //         result.push(data2);
+    //         return mock(300);
+    //     })
+    //     .then((data3) => {
+    //         result.push(data3);
+    //         return result;
+    //     });
+    return Promise.all([
+        mock(FIRST_DELAY),
+        mock(SECOND_DELAY),
+        mock(THIRD_DELAY)
+    ]);
+}
+
+// 3.
+// исправь функцию так чтобы блок try/catch обрабатывал завершенный с ошибкой промис
+// function catchExeption() {
+//     try {
+//         Promise.reject(new Error('my error'));
+//     } catch (err) {
+//         return err.message;
+//     }
+// }
+export async function catchExeption(): Promise<string | undefined> {
+    try {
+        await Promise.reject(new Error('my error'));
+    } catch (err: unknown) {
+        const e = err as Error;
+        return e.message;
+    }
 }
