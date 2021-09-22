fe95edf664d2feccb5ce14416c8f0f58b5490970
vv.shumilov <vv.shumilov@tensor.ru>
2021-09-22 14:30:23 +0300

    +

--- src/lesson-5-10/Cycles.ts
+++ src/lesson-5-10/Cycles.ts
@@ -1,9 +1,9 @@
 /*
-  В функцию forCycle() приходят два целых неотрицательных числа.
+  В функцию rangeSum() приходят два целых неотрицательных числа.
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function forCycle(start: number, end: number): number {
+export function rangeSum(start: number, end: number): number {
     let result = 0;
     
     for (let i=start; i<=end; i++) {
@@ -17,11 +17,11 @@ export function forCycle(start: number, end: number): number {
   
   
   /*
-    В функцию whileCycle() приходит неотрицательное число.
+    В функцию iterationCount() приходит неотрицательное число.
     Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
     и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
   */
-  export function whileCycle(a: number): number {
+  export function iterationCount(a: number): number {
     let result = 0;
   
     while (a > 0.1) {
@@ -34,11 +34,11 @@ export function forCycle(start: number, end: number): number {
   
   
   /*
-    В функцию doWhileCycle() приходит строка текста.
+    В функцию symbolsReplace() приходит строка текста.
     Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
     и верните итоговый результат.
   */
-  export function doWhileCycle(message: string): string {
+  export function symbolsReplace(message: string): string {
     let result = '';
     let i = 0;
   
