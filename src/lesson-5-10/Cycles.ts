6ffb9f3508896f98e37f187d901f9af4145c6c95
vv.shumilov <vv.shumilov@tensor.ru>
2021-09-22 14:21:57 +0300

    https://online.sbis.ru/opendoc.html?guid=a394cc38-d41a-45df-b160-b66a32e23a5c

---
+++ src/lesson-5-10/Cycles.ts
@@ -0,0 +1,56 @@
+/*
+  В функцию forCycle() приходят два целых неотрицательных числа.
+  Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
+  и верните итоговый результат.
+*/
+export function forCycle(start: number, end: number): number {
+    let result = 0;
+    
+    for (let i=start; i<=end; i++) {
+        if (i % 2 == 0) {
+            result += i;
+        }
+    }
+  
+    return result;
+  }
+  
+  
+  /*
+    В функцию whileCycle() приходит неотрицательное число.
+    Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
+    и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
+  */
+  export function whileCycle(a: number): number {
+    let result = 0;
+  
+    while (a > 0.1) {
+        a /= 2;
+        result++;
+    }
+  
+    return result;
+  }
+  
+  
+  /*
+    В функцию doWhileCycle() приходит строка текста.
+    Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
+    и верните итоговый результат.
+  */
+  export function doWhileCycle(message: string): string {
+    let result = '';
+    let i = 0;
+  
+    do {
+        if ((i+1) % 3 == 0) {
+            result += '_';
+        } else {
+            result += message[i];
+        }
+        i++;
+    } while (i < message.length);
+  
+    return result;
+  }
+  
