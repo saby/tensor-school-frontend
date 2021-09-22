e5cacd798d1df10b0ff0a9dfe3f680eaba4e0ad6
dm.kabanov <dm.kabanov@tensor.ru>
2021-09-22 18:45:30 +0300

    https://online.sbis.ru/opendoc.html?guid=a4739ec0-9c18-4898-ab96-a4d431685f67

--- src/lesson-9-1/task.test.ts
+++ src/lesson-9-1/task.test.ts
@@ -1,14 +1,14 @@
 import {getUsers, createUser} from './task';
 
 describe('Работа с данными', () => {
-   it('Задача 1. GET запрос', () => {
+   it('Задача 5. GET запрос', () => {
       return getUsers().then((data) => {
          expect(data.length).toBe(10);
          expect(typeof data[0]).toBe('string');
       });
    });
 
-   it('Задача 2. POST запрос', () => {
+   it('Задача 6. POST запрос', () => {
       return createUser({name: 'Ivan'}).then((res) => {
          expect(res).toEqual({name: 'Ivan', id: 11});
       });
--- src/lesson-9-1/task.ts
+++ src/lesson-9-1/task.ts
@@ -1,14 +1,19 @@
+interface IUser {
+   id: number;
+   name: string;
+}
+
 export async function getUsers(): Promise<string[]> {
    const result: string[] = [];
    const response = await fetch('https://jsonplaceholder.typicode.com/users');
    const users = await response.json();
-   users.forEach((user) => {
+   users.forEach((user: IUser) => {
       result.push(user.name);
    });
    return result;
 }
 
-export async function createUser(user: Record<string, unknown>): Promise<Record<string, unknown>> {
+export async function createUser(user: object): Promise<object & {id: number}> {
    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
       method: 'POST',
       body: JSON.stringify(user),
