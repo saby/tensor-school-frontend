aac6bbf2ae3239ec1346ea427a6d07bc2f199208
vv.shumilov <vv.shumilov@tensor.ru>
2021-09-22 14:47:49 +0300

    +

--- src/lesson-5-10/Cycles.ts
+++ src/lesson-5-10/Cycles.ts
@@ -13,15 +13,15 @@ export function rangeSum(start: number, end: number): number {
     }
   
     return result;
-  }
+}
   
   
-  /*
-    В функцию iterationCount() приходит неотрицательное число.
-    Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
-    и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
-  */
-  export function iterationCount(a: number): number {
+/*
+  В функцию iterationCount() приходит неотрицательное число.
+  Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
+  и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
+*/
+export function iterationCount(a: number): number {
     let result = 0;
   
     while (a > 0.1) {
@@ -30,15 +30,15 @@ export function rangeSum(start: number, end: number): number {
     }
   
     return result;
-  }
+}
   
   
-  /*
-    В функцию symbolsReplace() приходит строка текста.
-    Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
-    и верните итоговый результат.
-  */
-  export function symbolsReplace(message: string): string {
+/*
+  В функцию symbolsReplace() приходит строка текста.
+  Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
+  и верните итоговый результат.
+*/
+export function symbolsReplace(message: string): string {
     let result = '';
     let i = 0;
   
@@ -52,5 +52,4 @@ export function rangeSum(start: number, end: number): number {
     } while (i < message.length);
   
     return result;
-  }
-  
+}
