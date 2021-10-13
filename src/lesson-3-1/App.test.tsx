20bb7810b5e96c72e442c05f250991fd4567d64c
as.zaycev <as.zaycev@tensor.ru>
2021-10-13 10:09:20 +0300

    безголовый prettier и начало отказа от реакта

--- src/lesson-3-1/App.test.tsx
+++ src/lesson-3-1/App.test.tsx
@@ -1,40 +1,50 @@
 import App from './App';
-import {render} from '@testing-library/react';
-const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
-const puppeteer = require('puppeteer');
+import { render } from '@testing-library/react';
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import puppeteer, { Page, Browser } from 'puppeteer';
+import path from 'path';
 
 const customConfig = { threshold: 0 };
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
-  customDiffConfig: customConfig,
-  noColors: true,
-});
-let browser;
-
-beforeAll(async () => {
-  browser = await puppeteer.launch();
+    customDiffConfig: customConfig,
+    noColors: true
 });
 expect.extend({ toMatchImageSnapshot });
 
-describe('Сетка', () => {
-   it('Верстка не тронута', () => {
-      const {container} = render(<App/>);
-      expect(container).toMatchSnapshot();
-   });
+let browser: Browser;
 
-   async function removeBanners(page){
-      await page.evaluate(() => {
-        (document.querySelectorAll('.grid-container') || []).forEach(el => el.remove());
-      });
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+        headless: true
+    });
+});
+
+afterAll(async () => {
+    await browser.close();
+});
+
+describe('Сетка', () => {
+    it('Верстка не тронута', () => {
+        const { container } = render(<App />);
+        expect(container).toMatchSnapshot();
+    });
+
+    async function removeBanners(page: Page) {
+        await page.evaluate(() => {
+            document.querySelectorAll('.grid-container').forEach((el) =>
+                el.remove()
+            );
+        });
     }
 
     it('renders correctly', async () => {
-      const page = await browser.newPage();
-      await page.goto('http://localhost:3000');
+        const page = await browser.newPage();
+        await page.goto(`file:${path.join(__dirname, 'index.html')}`);
 
-      await removeBanners(page);
+        await removeBanners(page);
 
-      const image = await page.screenshot();
+        const image = await page.screenshot();
 
-      expect(image).toMatchImageSnapshot();
+        expect(image).toMatchImageSnapshot();
     });
 });
