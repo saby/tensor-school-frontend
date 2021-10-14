2611514452c33b30b34c79c22800af0d2bddb045
aa.pritykin <aa.pritykin@tensor.ru>
2021-10-14 19:00:14 +0300

    https://online.sbis.ru/opendoc.html?guid=ff441a68-24c4-432f-96a8-e07583a2a0ca

---
+++ src/lesson-6-38/accessors.ts
@@ -0,0 +1,55 @@
+// 1
+export interface IToDoItem {
+   caption: string;
+   done?: boolean;
+}
+
+export type TListOption = (string | IToDoItem )[];
+
+export class ToDo {
+   protected _list: IToDoItem[] = [];
+
+   constructor(list?: TListOption) {
+      this._list = list ? this._getList(list) : [];
+   }
+
+   clear(): void {
+      this._list = [];
+   }
+
+   private _getList(list: TListOption): IToDoItem[] {
+      const added = list.map(item => {
+         if (typeof item === 'string') {
+            return {
+               caption: item, done: false
+            };
+         }
+         if (typeof item === 'object' && item.caption) {
+            return {
+               caption: item.caption,
+               done: item.done || false
+            };
+         }
+
+         // for tslint fix
+         return {caption: '', done: false};
+      }).filter(item => item.caption);
+
+      return [...this._list, ...added];
+   }
+
+   set done(caption: string) {
+      const doneItem = this._list.find(item => item.caption === caption);
+      if (doneItem) {
+         doneItem.done = true;
+      }
+   }
+
+   get list(): string[] {
+      return this._list.map(item => `${item.caption}, ${(item.done ? '+' : '-')}`);
+   }
+
+   set list(list: TListOption) {
+      this._list = this._getList(list);
+   }
+}
